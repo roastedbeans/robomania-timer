841dfbefd1c015d549ab2e6f835766cb387bc126
Vince <vinceabella05@gmail.com>
2023-03-25 20:00:21 +0800

    fonts fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "src/assets/LOGOMain.png";
-
+import Logo from "../assets/logo.png";
 export const Header = () => {
   return (
     <>
       <img
         className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
-        src="/src/assets/LOGOMain.png"></img>
+        src={Logo}></img>
     </>
   );
 };
