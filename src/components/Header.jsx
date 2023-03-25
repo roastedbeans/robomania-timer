c28ed43ad3e737069747607f5eb0bb2f875edc89
Vince <vinceabella05@gmail.com>
2023-03-26 00:11:36 +0800

    ui update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ export const Header = () => {
     <>
       <div className="header-container flex gap-0 w-[100%] justify-evenly align-middle drop-shadow- xl">
         <img
-          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          className="relative content-center z-10 h-[9rem] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
           src={CpED}></img>
         <img
-          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          className="relative content-center z-10 h-[10rem] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
           src={Logo}></img>
         <img
-          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          className="relative content-center z-10 h-[10rem] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
           src={CpEC}></img>
       </div>
     </>
