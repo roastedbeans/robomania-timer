8dc5793bcf3dd2ab649e3ee529b711c4241057c0
Vince <vinceabella05@gmail.com>
2023-03-25 20:36:17 +0800

    logo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Logo from "../assets/LOGOMain.png";
+import CpED from "../assets/CpED.png";
+import CpEC from "../assets/CpEC.png";
+
 export const Header = () => {
   return (
     <>
-      <img
-        className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
-        src={Logo}></img>
+      <div className="header-container flex gap-0 w-[100%] justify-evenly align-middle">
+        <img
+          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          src={CpED}></img>
+        <img
+          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          src={Logo}></img>
+        <img
+          className="relative content-center z-10 h-[20vh] top-[-2vh] p-0 m-0 w-full object-scale-down drop-shadow-lg"
+          src={CpEC}></img>
+      </div>
     </>
   );
 };
